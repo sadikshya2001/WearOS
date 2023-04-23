3405a1e3dafec5f543e24cd3f85de8db47367e05
Sadikshya Satyal <sadikshyasatyal50@gmail.com>
2023-04-23 11:43:23 +0300

    -real time -image of the weather

--- app/src/main/java/com/example/wearos/MainActivity.kt
+++ app/src/main/java/com/example/wearos/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.wearos
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.TextView
 import com.example.wearos.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : Activity() {
 
@@ -13,6 +16,19 @@ class MainActivity : Activity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val textView = TextView(this)
+        textView.text = "08/04 SAT ~ San Francisco"
+        textView.gravity = Gravity.TOP or Gravity.CENTER_HORIZONTAL
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Set the time text
+        val calendar = Calendar.getInstance()
+        val hour = calendar.get(Calendar.HOUR_OF_DAY)
+        val minute = calendar.get(Calendar.MINUTE)
+        binding.timeText.text = String.format("%02d:%02d", hour, minute)
 
     }
 }
